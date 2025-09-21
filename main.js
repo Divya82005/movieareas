ccbd4a30f69d228ca054f4d266aff9c69450e72f
Divya82005 <divyasridinakaran@gmail.com>
2025-09-21 18:32:20 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,26 @@ app.get('/', (req, res) => {
   res.json({msg : "hello world"});
 }); 
 
+//CRUD fuctionality of movies
+//R- for Reading
+app.get('/movies',() =>{
+
+})
+
+//C-for creating movies
+app.post('/movies',()=> {
+
+})
+
+//U - for updating movies
+app.put('/movies/:id',()=>{
+
+})
+
+//D- for Deleting movies
+app.delete('/movies/:id', ()=> {
+  
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
